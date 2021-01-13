af6d8e2234f0c7f3e0c2a257cf23a66fab089203
bokuhe <bokuhe@gmail.com>
2021-01-14 01:29:36 +0900

    Add biometric authentication interface

---
+++ src/utils/bio.ts
@@ -0,0 +1,61 @@
+import * as LocalAuthentication from 'expo-local-authentication'
+
+enum BiometricType {
+  NONE,
+  FACE,
+  FINGERPRINT,
+}
+
+export const isSupportedBiometricAuthentication = async (): Promise<boolean> => {
+  const hasHardware = await LocalAuthentication.hasHardwareAsync()
+  const supportedAuthenticationTypes = await LocalAuthentication.supportedAuthenticationTypesAsync()
+  const isEnrolled = await LocalAuthentication.isEnrolledAsync()
+
+  return (
+    hasHardware &&
+    supportedAuthenticationTypes.length > 0 &&
+    isEnrolled
+  )
+}
+
+export const getSupportedType = async (): Promise<BiometricType> => {
+  if (!(await isSupportedBiometricAuthentication())) {
+    return BiometricType.NONE
+  }
+
+  const supportedAuthenticationTypes = await LocalAuthentication.supportedAuthenticationTypesAsync()
+  if (supportedAuthenticationTypes.length > 0) {
+    if (
+      supportedAuthenticationTypes.includes(
+        LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION
+      )
+    ) {
+      return BiometricType.FACE
+    } else if (
+      supportedAuthenticationTypes.includes(
+        LocalAuthentication.AuthenticationType.FINGERPRINT
+      )
+    ) {
+      return BiometricType.FINGERPRINT
+    }
+  }
+
+  return BiometricType.NONE
+}
+
+export const authenticateBiometric = async ({
+  promptMessage,
+  cancelLabel,
+}: {
+  promptMessage: string
+  cancelLabel: string
+}): Promise<boolean> => {
+  const ret = await LocalAuthentication.authenticateAsync({
+    promptMessage,
+    cancelLabel,
+    disableDeviceFallback: true,
+    fallbackLabel: '',
+  })
+
+  return ret.success
+}
