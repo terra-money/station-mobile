2c831774ce5c39706428a694505eb4fc24a5e0cf
blue <skqksh@gmail.com>
2021-01-17 22:29:36 +0900

    Set font to components/Text, eslint fix

--- src/utils/native.ts
+++ src/utils/native.ts
@@ -1,10 +1,11 @@
 import { NativeModules } from 'react-native'
+import dev from './dev'
 
 export const isDeviceRooted = async (): Promise<boolean> => {
   try {
     return await NativeModules.RootChecker.isDeviceRooted()
   } catch (e) {
-    console.log(e)
+    dev.log(e)
   }
 
   return true
