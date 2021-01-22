2bd6b989f58ad176d586006c1456b4844203b137
blue <blue@terra.money>
2021-01-22 15:38:21 +0900

    Wallet Tab. pull to refresh and auto refresh after send asset or onfocus

--- src/screens/walletTab/Wallet/index.tsx
+++ src/screens/walletTab/Wallet/index.tsx
@@ -1,6 +1,8 @@
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { useSetRecoilState } from 'recoil'
+import { StackScreenProps } from '@react-navigation/stack'
 
+import { RootStackParams } from 'types'
 import { navigationHeaderOptions } from 'components/layout/TabScreenHeader'
 import Body from 'components/layout/Body'
 import WithAuth from 'components/layout/WithAuth'
@@ -11,15 +13,31 @@ import { useSwapRate } from 'hooks/useSwapRate'
 import SwapRateStore from 'stores/SwapRateStore'
 import History from './History'
 
-const Screen = (): ReactElement => {
+type Props = StackScreenProps<RootStackParams, 'Wallet'>
+
+const Screen = ({ navigation }: Props): ReactElement => {
   const { loading, data } = useSwapRate()
   const setSwapRate = useSetRecoilState(SwapRateStore.swapRate)
+
+  const [refreshing, setRefreshing] = useState(false)
+  const refreshPage = async (): Promise<void> => {
+    setRefreshing(true)
+    setTimeout(() => {
+      setRefreshing(false)
+    }, 100)
+  }
 
   useEffect(() => {
     if (false === loading && data) {
       setSwapRate(data)
     }
   }, [loading, data?.length])
+
+  useEffect(() => {
+    navigation.addListener('focus', () => {
+      refreshPage()
+    })
+  }, [])
 
   return (
     <WithAuth>
@@ -28,10 +46,15 @@ const Screen = (): ReactElement => {
           theme={'sky'}
           scrollable
           containerStyle={{ paddingTop: 20 }}
+          onRefresh={refreshPage}
         >
-          <WalletAddress user={user} />
-          <AvailableAssets user={user} />
-          <History user={user} />
+          {refreshing ? null : (
+            <>
+              <WalletAddress user={user} />
+              <AvailableAssets user={user} />
+              <History user={user} />
+            </>
+          )}
         </Body>
       )}
     </WithAuth>
