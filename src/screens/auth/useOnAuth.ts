583e091ed625e84aa5568167622c0bdc54f5284d
blue <blue@terra.money>
2021-01-26 18:26:51 +0900

    fix to remain login state

---
+++ src/screens/auth/useOnAuth.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useAuth } from 'use-station/src'
+import { settings } from 'utils/storage'
+
+export default (): void => {
+  const { goBack } = useNavigation()
+  const { user } = useAuth()
+
+  useEffect(() => {
+    user ? settings.set({ user }) : settings.delete(['user'])
+    user && goBack()
+  }, [user])
+}
