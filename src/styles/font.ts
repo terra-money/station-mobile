d0e6baa2b68955f27969361f13edbf46bbdce78e
Henry Park <henry@terra.money>
2021-01-28 13:06:05 +0900

    Add topup bio authentication, code cleanup

---
+++ src/styles/font.ts
@@ -0,0 +1,15 @@
+const gotham = {
+  book: 'Gotham-Book',
+  light: 'Gotham-Light',
+  medium: 'Gotham-Medium',
+  bold: 'Gotham-Bold',
+}
+
+const variant = {
+  tabularNums: 'tabular-nums',
+}
+
+export default {
+  gotham,
+  variant,
+}
