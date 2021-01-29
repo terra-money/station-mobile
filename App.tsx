a8ebcdaab48ad429b404025527ce58abb84e97db
Henry Park <henry@terra.money>
2021-01-29 16:51:21 +0900

    Add CodePush

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect, ReactElement } from 'react'
-import { StatusBar } from 'react-native'
+import {
+  AppState,
+  AppStateStatus,
+  Platform,
+  StatusBar,
+} from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import SplashScreen from 'react-native-splash-screen'
 import { RecoilRoot } from 'recoil'
@@ -27,6 +32,7 @@ import AlertModal, {
 import Drawer, {
   useDrawerState,
 } from 'components/onlyForApp.tsx/Drawer'
+import Update from './src/screens/Update'
 
 import networks from './networks'
 
@@ -51,8 +57,65 @@ let App = ({
   const { name: currentChain = '' } = currentChainOptions
 
   /* onboarding */
-
   const [showOnBoarding, setshowOnBoarding] = useState<boolean>(false)
+
+  /* update */
+  const [updateAvailable, setUpdateAvailable] = useState<boolean>()
+  const [receivedBytes, setReceivedBytes] = useState<number>(0)
+  const [totalBytes, setTotalBytes] = useState<number>(0)
+
+  useEffect(() => {
+    CodePush.sync(
+      {
+        updateDialog: undefined,
+        installMode: CodePush.InstallMode.IMMEDIATE,
+      },
+      (status) => {
+        switch (status) {
+          case CodePush.SyncStatus.UP_TO_DATE:
+            setUpdateAvailable(false)
+            break
+          case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+            break
+          case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+            setUpdateAvailable(true)
+            break
+          case CodePush.SyncStatus.INSTALLING_UPDATE:
+            setUpdateAvailable(true)
+            break
+          case CodePush.SyncStatus.UPDATE_INSTALLED:
+            setUpdateAvailable(true)
+            break
+        }
+      },
+      ({ receivedBytes, totalBytes }) => {
+        setReceivedBytes(receivedBytes)
+        setTotalBytes(totalBytes)
+      }
+    )
+  }, [])
+
+  const checkUpdate = async (): Promise<void> => {
+    const update = await CodePush.checkForUpdate()
+    if (update) {
+      setUpdateAvailable(true)
+    } else {
+      setUpdateAvailable(false)
+    }
+  }
+
+  useEffect(() => {
+    Platform.OS === 'android' && checkUpdate()
+
+    const activeListener = (state: AppStateStatus): void => {
+      state === 'active' && checkUpdate()
+    }
+
+    AppState.addEventListener('change', activeListener)
+    return (): void => {
+      AppState.removeEventListener('change', activeListener)
+    }
+  }, [])
 
   /* auth */
   const auth = useAuthState(user)
@@ -63,37 +126,47 @@ let App = ({
   useEffect(() => {
     const checkShowOnboarding = async (): Promise<void> => {
       setshowOnBoarding(false === (await getSkipOnboarding()))
-      SplashScreen.hide()
     }
     checkShowOnboarding()
   }, [])
 
+  useEffect(() => {
+    updateAvailable !== undefined && SplashScreen.hide()
+  }, [updateAvailable])
+
   return (
     <>
-      {showOnBoarding ? (
-        <OnBoarding setshowOnBoarding={setshowOnBoarding} />
-      ) : (
-        ready && (
-          <AppProvider value={{ drawer, modal, alertModal }}>
-            <ConfigProvider value={config}>
-              <AuthProvider value={auth}>
-                <SafeAreaProvider>
-                  <StatusBar
-                    barStyle="dark-content"
-                    backgroundColor="transparent"
-                    translucent={false}
+      {ready && updateAvailable !== undefined && (
+        <AppProvider value={{ drawer, modal, alertModal }}>
+          <ConfigProvider value={config}>
+            <AuthProvider value={auth}>
+              <SafeAreaProvider>
+                {updateAvailable ? (
+                  <Update
+                    receivedBytes={receivedBytes}
+                    totalBytes={totalBytes}
                   />
-                  <RecoilRoot>
-                    <AppNavigator />
-                    <AppModal modal={modal} />
-                    <Drawer drawer={drawer} />
-                    <AlertModal modal={alertModal} />
-                  </RecoilRoot>
-                </SafeAreaProvider>
-              </AuthProvider>
-            </ConfigProvider>
-          </AppProvider>
-        )
+                ) : showOnBoarding ? (
+                  <OnBoarding setshowOnBoarding={setshowOnBoarding} />
+                ) : (
+                  <>
+                    <StatusBar
+                      barStyle="dark-content"
+                      backgroundColor="transparent"
+                      translucent={false}
+                    />
+                    <RecoilRoot>
+                      <AppNavigator />
+                      <AppModal modal={modal} />
+                      <Drawer drawer={drawer} />
+                      <AlertModal modal={alertModal} />
+                    </RecoilRoot>
+                  </>
+                )}
+              </SafeAreaProvider>
+            </AuthProvider>
+          </ConfigProvider>
+        </AppProvider>
       )}
     </>
   )
