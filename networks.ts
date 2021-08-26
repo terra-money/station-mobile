7567eaa8f3bc38e7c7e91dbbb2f5755ab3a5c168
blue <blue@terra.money>
2021-08-26 22:29:46 +0900

    Set bombay chainId 0008 to 10

--- networks.ts
+++ networks.ts
@@ -8,20 +8,20 @@ enum NetworkEnum {
 
 const network: Record<NetworkEnum, ChainOptions> = {
   mainnet: {
-    name: 'mainnet', // Don't change. It's related with SHUTTLES valiable in use-station/useSend.
+    name: 'mainnet',
     chainID: 'columbus-4',
     lcd: 'https://lcd.terra.dev',
     fcd: 'https://fcd.terra.dev',
   },
   tequila: {
-    name: 'testnet', // Don't change. It's related with SHUTTLES valiable in use-station/useSend.
+    name: 'testnet',
     chainID: 'tequila-0004',
     lcd: 'https://tequila-lcd.terra.dev',
     fcd: 'https://tequila-fcd.terra.dev',
   },
   bombay: {
     name: 'bombay',
-    chainID: 'bombay-0008',
+    chainID: 'bombay-10',
     lcd: 'https://bombay-lcd.terra.dev',
     fcd: 'https://bombay-fcd.terra.dev',
   },
