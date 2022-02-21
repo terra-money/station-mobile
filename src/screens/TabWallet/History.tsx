6c5d887adb86261fb649d87ee16a4f7637415f0f
Alessandro <54709706+alecande11@users.noreply.github.com>
2022-02-21 13:08:59 +0100

    Fix history error (#17)

--- src/screens/TabWallet/History.tsx
+++ src/screens/TabWallet/History.tsx
@@ -72,7 +72,8 @@ const History = ({ user }: { user: User } & Props): ReactElement => {
 
   return loading ? (
     <Loading />
-  ) : error ? (
+  // @ts-expect-error
+  ) : error && !(error.response && error.response.status === 500) ? (
     <ErrorComponent />
   ) : walletTabUi ? (
     <RenderList ui={walletTabUi} />
