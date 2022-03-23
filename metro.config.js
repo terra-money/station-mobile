7960fdc895cf6edccf826fd8f1f0f2205c525e4c
steven4354 <steven4354@gmail.com>
2022-03-22 21:36:18 -0700

    panther example working

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,41 @@
-module.exports = {
-  resolver: {
-    extraNodeModules: require('node-libs-react-native'),
-  },
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getDefaultConfig } = require('metro-config')
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+module.exports = (async () => {
+  console.log(await getDefaultConfig())
+
+  const {
+    resolver: { assetExts },
+  } = await getDefaultConfig()
+
+  console.log(assetExts)
+
+  return {
+    resolver: {
+      extraNodeModules: require('node-libs-react-native'),
+      assetExts: [
+        ...assetExts,
+        'obj',
+        'mtl',
+        'JPG',
+        'vrx',
+        'hdr',
+        'gltf',
+        'glb',
+        'bin',
+        'arobject',
+        'gif',
+      ],
+    },
+    transformer: {
+      // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+  }
+})()
