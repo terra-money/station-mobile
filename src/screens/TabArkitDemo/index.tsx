f6666e97fdfbda7b298faff0ee72414c20addf00
steven4354 <steven4354@gmail.com>
2022-03-22 19:26:26 -0700

    hello world demo working

--- src/screens/TabArkitDemo/index.tsx
+++ src/screens/TabArkitDemo/index.tsx
@@ -1,10 +1,61 @@
-import React, { ReactElement } from 'react'
-import { navigationHeaderOptions } from "components/layout/Header"
+import React, { ReactElement, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import {
+  ViroARScene,
+  ViroText,
+  ViroConstants,
+  ViroARSceneNavigator,
+} from '@viro-community/react-viro'
+
+import { navigationHeaderOptions } from 'components/layout/Header'
+
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState('Initializing AR...')
+
+  function onInitialized(state, reason) {
+    console.log('guncelleme', state, reason)
+    if (state === ViroConstants.TRACKING_NORMAL) {
+      setText('Hello World!')
+    } else if (state === ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+
+  return (
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+    </ViroARScene>
+  )
+}
 
 export const ArkitDemo = (): ReactElement => {
-  return <></>
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  )
 }
 
 ArkitDemo.navigationOptions = navigationHeaderOptions({
-  title: 'Staking',
+  title: 'Arkit Demo',
+})
+
+var styles = StyleSheet.create({
+  f1: { flex: 1 },
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
 })
