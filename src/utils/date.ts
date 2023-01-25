4f2b98990bb0e131ed3c712f365ce9abb1a1ee34
Francisco <francisco.gomez@terra.money>
2023-01-25 09:11:54 -0600

    feat: remove unused files & deps

--- src/utils/date.ts
+++
@@ -1,6 +0,0 @@
-// to get format date like 'YYYY.MM.DD'
-// check testcase in __test__
-export const getDateYMD = (date: string): string => {
-  const match = date.match(/^(\d){4}\.(\d){2}\.(\d){2}/g)
-  return match ? match[0] : ''
-}
